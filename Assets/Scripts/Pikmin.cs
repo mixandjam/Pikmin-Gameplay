fa2fe09ea05a9cfac0e2727f12931877c28607b9
André Cardoso <amc.acardoso@gmail.com>
2020-05-06 22:42:56 +0200

    pikmin procedural motion fix

--- Assets/Scripts/Pikmin.cs
+++ Assets/Scripts/Pikmin.cs
@@ -26,8 +26,6 @@ public class Pikmin : MonoBehaviour
     }
     public void SetTarget(Transform target, float updateTime = 1f)
     {
-        agent.acceleration = 8;
-
         if (state == State.Interact)
         {
             transform.parent = null;
@@ -78,7 +76,6 @@ public class Pikmin : MonoBehaviour
         transform.DOJump(target, 2, 1, time).SetDelay(delay).SetEase(Ease.Linear).OnComplete(() =>
         {
             agent.angularSpeed = 2000;
-            agent.acceleration = 25;
             agent.enabled = true;
             isFlying = false;
             CheckInteraction();
